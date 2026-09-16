aa93d02a776b9edc676309bac4611652622f5307
LTP22 <ltp022@gmail.com>
2026-09-16 14:41:40 -0300

    chore: esqueleto Spring Boot API REST desde start.spring.io

---
+++ LT-taller-git-2026/src/main/java/py/edu/uc/lp3/LT_taller_git_2026/LtTallerGit2026Application.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.LT_taller_git_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtTallerGit2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtTallerGit2026Application.class, args);
+	}
+
+}
